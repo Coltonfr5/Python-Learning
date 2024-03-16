8dfd55cc1ca503ce10f84b01ceea81e1222c8359
Azzy <102493493+Coltonfr5@users.noreply.github.com>
2024-03-15 21:22:45 -0400

    Added the first python file.
    funy

---
+++ coltons code.py
@@ -0,0 +1,39 @@
+# The 'hello' var is used to change the print function.
+
+hello = "this is a variable function text"
+
+number1 = 1
+number2 = 2
+
+randomname = 2
+randomtext = "repeat 2 times or else"
+
+changeable = 0
+
+print(hello) #Prints the variable "hello"
+
+# Simple math equation :p
+if number1 + number1 == number2:
+    print(" ")
+    print("1 + 1 does indeed = 2")
+
+# A script to tell if number2 is bigger than number1
+if number2 > number1:
+    print(" ")
+    print("2 is bigger than 1")
+
+
+# Random script to repeat a print command a few times
+for number1 in range(randomname):
+    print(" ")
+    print(randomtext)
+
+
+
+# This script right here repeats 6 times and adds number1 to changeable and if it equals 5
+# it will print "you now equal 5"
+for number1 in range((5 + 1)):
+    changeable = + number1
+    print(changeable)
+    if changeable == 5:
+        print("you now equal 5")
